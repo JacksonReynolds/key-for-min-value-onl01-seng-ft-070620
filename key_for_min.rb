14f48241edd47770a2faccec10ea6e1600fc5a1f
Jackson Reynolds <jwreynold@gmail.com>
2020-07-09 02:47:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,10 @@
 
 def key_for_min_value(name_hash)
   min_key = nil
+  min_value = 1000
   name_hash.each do |key, value|
-    if value < min_key
-      min_key = value
+    if value < min_value
+      min_key = key
     end #if
   end #do
   min_key
